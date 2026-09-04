d5031d81c7575c29d8f86623f9bb1ff7a5b34e2e
Sebastian Gomez <seagomezar@gmail.com>
2026-09-04 11:21:03 -0500

    release: v1.1.0 (iOS build 5, Android versionCode 3)

--- FluteModesAndroid/app/build.gradle.kts
+++ FluteModesAndroid/app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         applicationId = "com.seagomezar.flutemodes"
         minSdk = 24
         targetSdk = 36
-        versionCode = 2
+        versionCode = 3
         versionName = "1.1.0"
     }
 
